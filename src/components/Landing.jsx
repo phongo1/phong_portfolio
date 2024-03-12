d385f33273eb35615f6ed17391d022bff1d37e86
Phong Le <le.phonghoa105@gmail.com>
2024-03-11 23:02:31 -0400

    3d model layout

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -11,8 +11,8 @@ const Landing = () => {
 
 
   return (
-    <section className={`text-white w-full h-auto pt-[11rem] flex flex-wrap ${styles.paddingX} mx-auto gap-x-16`} >
-      <motion.div className='pb-16 w-fit max-w-7xl h-[25rem] flex flex-col justify-center align-center gap-2 mx-auto'>
+    <section className={`text-white w-full h-auto pt-[11rem] flex flex-wrap ${styles.paddingX} mx-auto gap-x-16 justify-center`} >
+      <motion.div className='pb-16 w-fit h-[25rem] flex flex-col justify-center align-center gap-2'>
         <p className='text-4xl'>Hello, I'm</p>
         <span className='font-bold text-6xl bg-gradient-to-r from-[#4d52ff] to-[#cf3dfd] text-transparent bg-clip-text '>
           <Typewriter
@@ -28,7 +28,7 @@ const Landing = () => {
         <p className='text-base'>an incoming software engineer</p>
       </motion.div>
       
-      <div className='w-[45rem] h-[25rem] relative overflow-visible flex align-middle justify-center'>
+      <div className='w-[45rem] h-[25rem] relative overflow-visible flex align-middle justify-center md:left-28'>
         <Spline scene="https://prod.spline.design/QAGyEAONPrAsk4Gv/scene.splinecode"/>
       </div>
     </section>
