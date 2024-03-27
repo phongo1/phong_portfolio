640af763648e00e64d605c744a5fa089e5dddca0
Phong Le <le.phonghoa105@gmail.com>
2024-03-27 02:18:26 -0400

    updated asssets

--- src/assets/index.js
+++ src/assets/index.js
@@ -9,6 +9,9 @@ import uva_cs from "./uva_cs_logo.png"
 import ss_logo from "./ss_logo.png"
 import pho_icon from "./pho_icon.jpg"
 import game_logo from "./game_logo.png"
+import gradebuddy_logo from "./gradebuddy_logo.png"
+import joseweb_thumbnail from "./joseweb_thumbnail.png"
+import beach_portrait from "./portrait_beach.jpg"
 
 export {
     logo,
@@ -21,5 +24,8 @@ export {
     uva_cs,
     ss_logo,
     pho_icon,
-    game_logo
+    game_logo,
+    gradebuddy_logo,
+    joseweb_thumbnail,
+    beach_portrait,
 };
