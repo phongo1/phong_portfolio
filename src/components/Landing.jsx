f73f6cdb513e33dbfdbbeb6677e48f32f2c373fc
Phong Le <le.phonghoa105@gmail.com>
2024-03-27 03:24:08 -0400

    loader

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,20 +1,46 @@
-import { React, useState } from 'react'
+import { React, useState, useEffect } from "react";
 import Typewriter from "typewriter-effect";
-import { motion } from "framer-motion";
-import Spline from '@splinetool/react-spline';
+import { AnimatePresence, motion } from "framer-motion";
+import Spline from "@splinetool/react-spline";
 
-import { styles } from '../styles';
-
+import { styles } from "../styles";
 
 const Landing = () => {
   const [isLoading, setIsLoading] = useState(true);
 
+  useEffect(() => {
+    const timer = setTimeout(() => setIsLoading(false), 1000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  const loaderVariants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 },
+    exit: { opacity: 0, transition: { duration: 0.5 } } 
+  };
 
   return (
-    <section className={`text-white w-full h-auto pt-[11rem] flex flex-wrap ${styles.paddingX} mx-auto gap-x-16 justify-center`} >
-      <motion.div className='pb-16 w-fit h-[25rem] flex flex-col justify-center align-center gap-2'>
-        <p className='text-4xl'>Hello, I'm</p>
-        <span className='font-bold text-6xl bg-gradient-to-r from-[#4d52ff] to-[#cf3dfd] text-transparent bg-clip-text '>
+    <section
+      className={`text-white w-full h-auto pt-[11rem] flex flex-wrap ${styles.paddingX} mx-auto gap-x-16 justify-center`}
+    >
+      <AnimatePresence>
+        {isLoading && (
+          <motion.div
+            className="flex flex-col gap-2 justify-center items-center w-full h-full fixed z-40 bg-black top-0"
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+            variants={loaderVariants}
+            key="loader"
+          >
+            <div className="loader"></div>
+            <span className="w-fit">loading...</span>
+          </motion.div>
+        )}
+      </AnimatePresence>
+      <motion.div className="pb-16 w-fit h-[25rem] flex flex-col justify-center align-center gap-2">
+        <p className="text-4xl">Hello, I'm</p>
+        <span className="font-bold text-6xl bg-gradient-to-r from-[#4d52ff] to-[#cf3dfd] text-transparent bg-clip-text ">
           <Typewriter
             options={{
               strings: ["Phong"],
@@ -25,14 +51,19 @@ const Landing = () => {
             }}
           />
         </span>
-        <p className='text-base'>an incoming software engineer</p>
+        <p className="text-base">an incoming software engineer</p>
       </motion.div>
-      
-      <div className='w-[45rem] h-[25rem] relative overflow-visible flex align-middle justify-center md:left-28'>
-        <Spline scene="https://prod.spline.design/QAGyEAONPrAsk4Gv/scene.splinecode"/>
+
+      <div
+        className={`w-[45rem] h-[25rem] relative overflow-visible flex align-middle justify-center md:left-28 ${
+          isLoading ? "visibility-hidden" : ""
+        }`}
+      >
+        <Spline scene="https://prod.spline.design/QAGyEAONPrAsk4Gv/scene.splinecode" />
+        <div className="absolute mx-auto b w-60 h-80 top-[10%] shadow-2xl shadow-indigo-600/50 -z-10"></div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Landing
+export default Landing;
--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const ProjectCard = ({ project }) => {
   return (
     <div className="bg-[#1d1836] rounded-lg overflow-hidden hover:scale-110 hover:shadow-xl hover:shadow-indigo-600/50 transition-scale duration-300 ease-in-out w-[18rem] md:w-[23rem] flex flex-col items-center gap-4 shadow-md shadow-indigo-600/50">
-      <div className="w-full h-44 bg-white rounded-t-md overflow-hidden flex justify-center items-center">
+      <div className="w-full h-44 bg-white rounded-t-md overflow-hidden flex justify-center items-center ">
         <img
           src={project.image}
           alt={project.name}
