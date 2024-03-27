84df1485d5165d6e096b11027d2cb55980086392
Phong Le <le.phonghoa105@gmail.com>
2024-03-27 02:24:51 -0400

    resume update

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,7 +2,7 @@ import logo from "./logo.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
 import portrait from "./portrait.png"
-import phong_resume from "./phongle-resume.pdf"
+import phong_resume from "./phongle_resume.pdf"
 import spotlist from "./spotlist_logo.png"
 import aipi from "./aipi_logo.png"
 import uva_cs from "./uva_cs_logo.png"
