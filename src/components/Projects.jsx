c1523cc00e611cb660baa325ede7c9d63c18d9e3
Phong Le <le.phonghoa105@gmail.com>
2024-03-30 19:10:18 -0400

    modal animation speed

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -47,7 +47,7 @@ const Projects = () => {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
-            transition={{ duration: 1 }}
+            transition={{ duration: .4, ease:"easeIn" }}
             className="fixed"
           >
             <ProjectModal
